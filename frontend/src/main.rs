f9bffe2b36da70b52c4b1ed51505db95e8015e7f
Ben Belland <benbelland@gmail.com>
2023-10-08 16:50:56 -0700

    initial layout fiddling
    creates two blocks with borders and shows them for five seconds

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use tui::{
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders},
+    Frame,
+};
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+    .direction(Direction::Vertical)
+    .margin(1)
+    .constraints(
+    [
+        Constraint::Percentage(10),
+                 Constraint::Percentage(80),
+                 Constraint::Percentage(10)
+    ].as_ref()
+    )
+    .split(f.size());
+    let block = Block::default()
+    .title("Block 1")
+    .borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default()
+    .title("Block 2")
+    .borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
+}
+
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+  widgets::{Widget},
+  terminal::Terminal,
+};
+use crossterm:: {
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        ui(f);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    //restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
