928f0dc011c92bac71c06ee792263167af47740f
Ben Belland <benbelland@gmail.com>
2023-10-08 19:55:00 -0700

    creates a paragraph example in the third block
    getting some overflow of escape codes every once in a while
    Not sure what's causing it yet

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,9 @@
 use tui::{
     backend::Backend,
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders},
+    layout::{Constraint, Alignment, Direction, Layout},
+    widgets::{Paragraph, Block, Borders, Wrap},
+    style::{Color, Style, Modifier},
+    text::{Span,Spans},
     Frame,
 };
 fn ui<B: Backend>(f: &mut Frame<B>) {
@@ -11,8 +13,8 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     .constraints(
     [
         Constraint::Percentage(10),
-                 Constraint::Percentage(80),
-                 Constraint::Percentage(10)
+                 Constraint::Percentage(60),
+                 Constraint::Percentage(30)
     ].as_ref()
     )
     .split(f.size());
@@ -24,12 +26,26 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     .title("Block 2")
     .borders(Borders::ALL);
     f.render_widget(block, chunks[1]);
+
+    let text = vec![
+        Spans::from(vec![
+            Span::raw("This is a "),
+            Span::styled("test", Style::default().add_modifier(Modifier::ITALIC)),
+            Span::raw("."),
+        ]),
+        Spans::from(Span::styled("And only a test", Style::default().fg(Color::Red))),
+    ];
+    let final_block = Paragraph::new(text)
+        .block(Block::default().title("Paragraph").borders(Borders::ALL))
+        .alignment(Alignment::Center)
+        .wrap(Wrap {trim: true});
+    f.render_widget(final_block, chunks[2]);
+
 }
 
 use std::{io, thread, time::Duration};
 use tui::{
     backend::CrosstermBackend,
-  widgets::{Widget},
   terminal::Terminal,
 };
 use crossterm:: {
