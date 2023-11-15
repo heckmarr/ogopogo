02b20de35f06380a1c9cb56762e86613b0a9975d
Ben Belland <benbelland@gmail.com>
2023-11-14 18:00:03 -0800

    starting file write out

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, fs, time::Duration, time::Instant};
+use std::{io, fs, time::Duration, path, time::Instant};
 //Tui imports
 use tui::{
     backend::{Backend, CrosstermBackend},
@@ -191,9 +191,9 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, c: u16, r: u16, vector_colours: [[Vec3b; 40] ; 20], data_string: &str, delta_time: Instant) {
+    let path = path::Path::new("./skitters/skitter001");
 
-
-
+    fs::write(path, data_string);
 
     let chunks = Layout::default()
     .direction(Direction::Horizontal)
